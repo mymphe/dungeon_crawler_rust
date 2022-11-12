0820531603f8bfbae9157b16321357fa2a167cbc
Azat Serikov <azatsdev@gmail.com>
2022-11-12 16:16:03 +0600

    feat: camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -7,6 +8,9 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -14,12 +18,10 @@ mod prelude {
 
 use prelude::*;
 
-pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
-pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
@@ -29,16 +31,20 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
