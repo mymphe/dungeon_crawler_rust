9d8afbf2a6de6e9ddf37a4d179aedc0591dc1ca9
Azat Serikov <azatsdev@gmail.com>
2023-01-13 13:34:25 +0600

    feat: spawn monsters

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod camera;
 mod components;
-mod spawner;
 mod map;
 mod map_builder;
+mod spawner;
 mod systems;
 
 mod prelude {
@@ -20,9 +20,9 @@ mod prelude {
 
     pub use crate::camera::*;
     pub use crate::components::*;
-    pub use crate::spawner::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
+    pub use crate::spawner::*;
     pub use crate::systems::*;
 }
 
@@ -41,6 +41,12 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
 
