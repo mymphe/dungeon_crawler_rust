88593b3f60d47358e6bdcf91d4e366fbe935e856
Azat Serikov <azatsdev@gmail.com>
2023-01-15 23:44:03 +0600

    feat: turns

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -7,7 +7,8 @@ pub fn player_input(
     ecs: &mut SubWorld,
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState,
 ) {
     if let Some(key) = key {
         let delta = match key {
@@ -15,17 +16,17 @@ pub fn player_input(
             VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
-            _ => Point::new(0, 0)
+            _ => Point::new(0, 0),
         };
 
         if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
+            let mut players = <&mut Point>::query().filter(component::<Player>());
             players.iter_mut(ecs).for_each(|pos| {
                 let destination = *pos + delta;
                 if map.can_enter_tile(destination) {
                     *pos = destination;
-                    camera.on_player_move(destination)
+                    camera.on_player_move(destination);
+                    *turn_state = TurnState::PlayerTurn;
                 }
             })
         }
